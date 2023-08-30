807131f94190efa059b65abb71f64fe10d456cac
bzpovo <643809570@qq.com>
2023-08-30 16:22:22 +0800

    任务5.1-5.2

---
+++ frontend_puhui-master(init)/src/main/java/com/jd/service/ICompanyService.java
@@ -0,0 +1,151 @@
+package com.jd.service;
+
+import com.jd.bean.BankCard;
+import com.jd.bean.Company;
+import com.jd.bean.IdCard;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 添加公司用户注册方法
+ *
+ * @author zpbao
+ * @version 1.0.0
+ * @date 2023/08/30
+ */
+public interface ICompanyService {
+    /**
+     * 按邮箱登录
+     * @return
+     */
+    Company selectCompanyByEmail(String email, String psw);
+
+    /**
+     * 按电话登录
+     * @return
+     */
+    Company selectCompanyByPhone(String phone, String psw);
+
+    /**
+     * 新增用户信息
+     *
+     * @param company 用户信息
+     * @return 结果
+     */
+    int insertCompany(Company company);
+
+    /**
+     * 根据激活码查询用户信息
+     *
+     * @param remark 激活码
+     * @return 用户信息
+     */
+    Company selectCompanyByActiveCode(String remark);
+
+    /**
+     * 修改企业信息
+     *
+     * @param company 用户信息
+     * @return 结果
+     */
+    int updateCompany(Company company);
+
+    /**
+     * 检测企业编号
+     * @param comNo
+     * @return
+     */
+    int findCompanyComNo(String comNo);
+
+    /**
+     * 检测企业电话
+     * @param comPhone
+     * @return
+     */
+    int findCompanyComPhone(String comPhone);
+
+    /**
+     * 查询账号是否验证
+     * @param tel
+     * @return
+     */
+    int selectCompanyTelById(String tel);
+
+    /**
+     * 查询账号是否验证
+     * @param ema
+     * @return
+     */
+    int selectCompanyEmaById(String ema);
+
+    /**
+     * 按电话号码查
+     * @param pNo
+     * @return
+     */
+    Company selectCompanyLoginTel(String pNo);
+
+    /**
+     * 按邮箱查
+     * @param eNo
+     * @return
+     */
+    Company selectCompanyLoginEma(String eNo);
+
+    /**
+     * 修改密码
+     * @param psw
+     * @return
+     */
+    int updateCompanyPsw(int cId,String psw);
+
+    /**
+     * 修改认证状态
+     * @param cId
+     * @return
+     */
+    void updateCompanyAttState(int cId);
+
+    //人脸识别
+    int faceMatch(String pNo, String face, int status, Date date, int ComId);
+
+    /*风控0122_增加通过编号查询企业名称的方法*/
+    /**
+     * 通过企业编号查询企业名称
+     * @param comId
+     * @return
+     */
+    String selectCompanyByComId(int comId);
+
+    //查询人脸识别认证状态
+    String getFaceMatchState(String pNo);
+
+    String bankSave(String bankName, String bankCardNo, int comId);
+
+    String idCardSave(String userName, String idCard, String address, String birthday, String gender, String nation, String period, int comId);
+
+    String getComNo(String phoneNo);
+
+    void vite(int id, Date date);
+
+    int messagetj(String msg, String comId, Date date);
+
+    IdCard getIdCardByComId(int comId);
+
+    BankCard getBankCardByComId(int comId);
+
+    List<IdCard> getUserIdInfo(int comId);
+
+    String getStep(int comId);
+
+    int setStep(int comId);
+
+    int updateStep(String step, int comId);
+
+    int giveScore(int comId, int i, Date date);
+
+    int getcredit(int comId);
+
+    int updateloan(String file, String nid);
+}
---
+++ frontend_puhui-master(init)/src/main/java/com/jd/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,303 @@
+package com.jd.service.impl;
+
+import com.jd.bean.BankCard;
+import com.jd.bean.Company;
+import com.jd.bean.IdCard;
+import com.jd.common.service.MailService;
+import com.jd.dao.CompanyMapper;
+import com.jd.service.ICompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 实现公司用户注册方法
+ *
+ * @author zpbao
+ * @version 1.0.0
+ * @date 2023/08/30
+ */
+@Service
+public class CompanyServiceImpl implements ICompanyService {
+
+    @Resource
+    private CompanyMapper companyMapper;
+
+    @Autowired
+    private MailService mailService;
+
+    /**
+     * 按邮箱登录
+     *
+     * @param email
+     * @param psw
+     * @return
+     */
+    @Override
+    public Company selectCompanyByEmail(String email, String psw) {
+        return companyMapper.selectCompanyByEmail(email, psw);
+    }
+
+    /**
+     * 按电话登录
+     *
+     * @param phone
+     * @param psw
+     * @return
+     */
+    @Override
+    public Company selectCompanyByPhone(String phone, String psw) {
+        return companyMapper.selectCompanyByPhone(phone, psw);
+    }
+
+    /**
+     * 新增用户信息
+     *
+     * @param company 用户信息
+     * @return 结果
+     */
+    @Override
+    public int insertCompany(Company company) {
+        int i = companyMapper.insertCompany(company);
+        //获取激活码
+        String code = company.getComRemark();
+        //定义主题
+        String subject = "来自普惠金融的注册激活邮件";
+        //激活链接
+        String conText = "<h5>欢迎注册普惠金融账号，请点击下方链接进行邮箱账号激活</h5><a href=\"http://localhost:9090/company/checkCode?code=" + code + "\" >激活请点击:" + code + "</a>";
+        //发送激活邮件
+        mailService.sendMimeMail(company.getComEmail(), subject, conText);
+        return i;
+    }
+
+    /**
+     * 根据激活码查询用户信息
+     *
+     * @param remark 激活码
+     * @return 用户信息
+     */
+    @Override
+    public Company selectCompanyByActiveCode(String remark) {
+        return companyMapper.selectCompanyByActiveCode(remark);
+    }
+
+    /**
+     * 修改企业信息
+     *
+     * @param company 用户信息
+     * @return 结果
+     */
+    @Override
+    public int updateCompany(Company company) {
+        return companyMapper.updateCompany(company);
+    }
+
+    /**
+     * 检测企业编号
+     *
+     * @param comNo
+     * @return
+     */
+    @Override
+    public int findCompanyComNo(String comNo) {
+        return companyMapper.findCompanyComNo(comNo);
+    }
+
+    /**
+     * 检测企业电话
+     *
+     * @param comPhone
+     * @return
+     */
+    @Override
+    public int findCompanyComPhone(String comPhone){
+        return companyMapper.findCompanyComPhone(comPhone);
+    }
+
+    /**
+     * 查询账号是否验证
+     *
+     * @param tel
+     * @return
+     */
+    @Override
+    public int selectCompanyTelById(String tel) {
+        return companyMapper.selectCompanyTelById(tel);
+    }
+
+    /**
+     * 查询账号是否验证
+     *
+     * @param ema
+     * @return
+     */
+    @Override
+    public int selectCompanyEmaById(String ema) {
+        return companyMapper.selectCompanyEmaById(ema);
+    }
+
+    /**
+     * 按电话号码查
+     *
+     * @param pNo
+     * @return
+     */
+    @Override
+    public Company selectCompanyLoginTel(String pNo) {
+        return companyMapper.selectCompanyLoginTel(pNo);
+    }
+
+    /**
+     * 按邮箱查
+     *
+     * @param eNo
+     * @return
+     */
+    @Override
+    public Company selectCompanyLoginEma(String eNo) {
+        return companyMapper.selectCompanyLoginEma(eNo);
+    }
+
+    /**
+     * 修改密码
+     *
+     * @param cId
+     * @param psw
+     * @return
+     */
+    @Override
+    public int updateCompanyPsw(int cId, String psw) {
+        return companyMapper.updateCompanyPsw(cId, psw);
+    }
+
+    /**
+     * 修改认证状态
+     *
+     * @param cId
+     * @return
+     */
+    @Override
+    public void updateCompanyAttState(int cId) {
+        companyMapper.updateCompanyAttState(cId);
+    }
+
+
+    /*风控0122_XXX*/
+
+    /**
+     * 通过企业编号查询企业名称
+     * @param comId
+     * @return
+     */
+    @Override
+    public String selectCompanyByComId(int comId) {
+        return companyMapper.selectComByComId(comId);
+    }
+
+    @Override
+    public int faceMatch(String pNo, String face, int status, Date date, int comId) {
+
+        return companyMapper.faceMatch(pNo, face, status, date, comId);
+    }
+
+    @Override
+    public String getFaceMatchState(String pNo) {
+        int count =  companyMapper.getFaceMatchState(pNo);
+        if(count > 0){
+            return "已通过";
+        }else{
+            return "未认证";
+        }
+    }
+
+    @Override
+    public String bankSave(String bankName, String bankCardNo, int comId) {
+        int count = companyMapper.bankSave(bankName, bankCardNo, comId);
+        if(count > 0){
+            return "已添加成功";
+        }else{
+            return "未添加成功";
+        }
+    }
+
+    @Override
+    public String idCardSave(String userName, String idCard, String address, String birthday, String gender, String nation,  String period, int comId) {
+        int count = companyMapper.idCardSave(userName, idCard, address, birthday, gender, nation, period, comId);
+        System.out.println(period);
+        if(count > 0){
+            return "已添加成功";
+        }else{
+            return "未添加成功";
+        }
+    }
+
+    @Override
+    public String getComNo(String phoneNo) {
+        return companyMapper.getComNo(phoneNo);
+    }
+
+    @Override
+    public void vite(int id, Date date) {
+        int count = companyMapper.getViteCount(id);
+        if(count > 0){
+            companyMapper.vite(id,date);
+        }else{
+            companyMapper.viteAdd(id, date);
+        }
+
+    }
+
+    @Override
+    public int messagetj(String msg, String comId, Date date) {
+        return companyMapper.messagetj(msg, comId, date);
+    }
+
+    @Override
+    public IdCard getIdCardByComId(int comId) {
+        return companyMapper.getIdCardByComId(comId);
+    }
+
+    @Override
+    public BankCard getBankCardByComId(int comId) {
+        return companyMapper.getBankCardByComId(comId);
+    }
+
+    @Override
+    public List<IdCard> getUserIdInfo(int comId) {
+        return companyMapper.getUserIdInfo(comId);
+    }
+
+    @Override
+    public String getStep(int comId) {
+        return companyMapper.getStep(comId);
+    }
+
+    @Override
+    public int setStep(int comId) {
+        return companyMapper.setStep(comId);
+    }
+
+    @Override
+    public int updateStep(String step, int comId) {
+        return companyMapper.updateStep(step, comId);
+    }
+
+    @Override
+    public int giveScore(int comId, int i, Date date) {
+        return companyMapper.giveScore(comId, i, date);
+    }
+
+    @Override
+    public int getcredit(int comId) {
+        return companyMapper.getcredit(comId);
+    }
+
+    @Override
+    public int updateloan(String file, String nid) {
+        return companyMapper.updateloan(file, nid);
+    }
+
+}
